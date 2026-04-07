da1f40a6670a0f5f0ca82928e367410c803698ce
Yoga <yogaputra0107@gmail.com>
2026-04-07 19:33:13 +0700

    fix : cors

--- main.py
+++ main.py
@@ -48,7 +48,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000",
+        "https://dp-enterprise.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
